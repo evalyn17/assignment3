ac15b22028143e1f94a49d1e23a3ddb4dfac01d2
evalyn17 <156710606+evalyn17@users.noreply.github.com>
2024-03-27 09:42:09 -0400

    project

--- Account.py
+++ Account.py
@@ -27,5 +27,40 @@ class Account:
         else:
             self.chequingAccount.deposit(amount)
 
-accountNo = int(input("Enter Account Number: "))
-account_name = input("Enter Name: ")
+class checqueingAccount(Account):
+    def __init__(self):
+        self.balance = 500
+        self.overdraft_limit = 1000
+    
+    def withdraw(self, amount):
+        if amount <= self.balance + self.overdraft_limit:
+            self.balance -= amount
+            print("Current Balance: ", self.balance)
+        else:
+            print("You Do Not Have Enough Funds!")
+    
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print("Current Balance: ", self.balance)
+        else:
+            print("Invalid Amount!")
+
+class savingAccount(Account):
+    def __init__(self):
+        self.balance = 1000
+        self.min_balance = 200
+    
+    def withdraw(self, amount):
+        if amount - self.balance >= self.min_balance:
+            self.balance -= amount 
+            print("Current Balance: ", self.balance)
+        else:
+            print("You Do Not Have Enough Funds!")
+    
+    def deposit(self,amount):
+        if amount > 0:
+            self.balance += amount
+            print("Current Balance: ", self.balance)
+        else:
+            print("Invalid Amount!")
--- bank.py
+++ bank.py
@@ -1,13 +1,14 @@
-import Account
+from Account import Account, checqueingAccount, savingAccount
 
 class bank:
     bank_name = "TD"
 
     def __init__(self):
         self.list_account = []
-        self.search_account = ()
+        self.search_accounts = ()
     
-    def list_account(self):
+#Listiing the 5 different bank accounts 
+    def create_accounts(self):
         self.list_account.append(Account, 1001, "Bob")
         self.list_account.append(Account, 1002, "Alex")
         self.list_account.append(Account, 1003,"May")
@@ -17,4 +18,13 @@ class bank:
     def search_account(self, accountNo):
         for account in self.list_account:
             if account.accountNo == accountNo:
-                return account
+                return account 
+
+#Asking usert for input/bank info
+accountNo = int(input("Enter Account Number: "))
+bank = bank()
+account = bank.search_account(accountNo)
+if account:
+    account.profile()
+else:
+    print("Sorry! Account Not Found!")
