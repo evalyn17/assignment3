db5a3f96ae91cfdad133e8fb6b243b89662aa8d3
evalyn17 <156710606+evalyn17@users.noreply.github.com>
2024-03-27 10:57:30 -0400

    project

--- Account.py
+++ Account.py
@@ -13,6 +13,7 @@ class Account:
         print("Chequing Balance: ", self.chequeingAccount.balance)
         print("Savings Balance: ", self.savingAccount.balance)
     
+    #asking user to deposit/withdraw from savings or chequeings
     def withdraw(self, amount):
         choice = int(input("Choose 1 for Saving or 2 for Chequing"))
         if choice == 1:
@@ -27,11 +28,13 @@ class Account:
         else:
             self.chequeingAccount.deposit(amount)
 
+#stating limit and balance in chequeing
 class chequeingAccount(Account):
     def __init__(self):
         self.balance = 500
         self.overdraft_limit = 1000
-    
+
+#giving user the current balance for withdrawal/deposits in chequeings and if the transaction was successful or not
     def withdraw(self, amount):
         if amount <= self.balance + self.overdraft_limit:
             self.balance -= amount
@@ -46,11 +49,13 @@ class chequeingAccount(Account):
         else:
             print("Invalid Amount!")
 
+#stating limit and balance in savings 
 class savingAccount(Account):
     def __init__(self):
         self.balance = 1000
         self.min_balance = 200
-    
+
+#giving user the current balance for withdrawal/deposits in savings and if the transaction was successful or not
     def withdraw(self, amount):
         if amount - self.balance >= self.min_balance:
             self.balance -= amount 
--- bank.py
+++ bank.py
@@ -3,11 +3,12 @@ import Account, savingAccount, chequeingAccount
 class bank:
     bank_name = "TD"
 
+#Attributes for the bank class
     def __init__(self):
         self.list_account = []
         self.search_accounts = ()
     
-#Listiing the 5 different bank accounts 
+#Creating and listing the 5 different bank accounts 
     def create_accounts(self):
         self.list_account.append(Account, 1001, "Bob", savingAccount)
         self.list_account.append(Account, 1002, "Alex", chequeingAccount)
@@ -20,7 +21,7 @@ class bank:
             if account.accountNo == accountNo:
                 return account 
 
-#Asking user for input for withdrawal/deposit
+#Asking user for bank info for withdrawal/deposit
 accountNo = int(input("Enter Account Number: "))
 account_name = input("Enter Name: ")
 choice = int(input("Enter 1 for Savings or 2 for Chequeing: "))
@@ -42,6 +43,6 @@ amount = int(input("Enter The Amount: "))
 if transaction == 1:
     account.deposit(amount)
 elif transaction == 2:
-    account.withdrawal(amount)
+    account.withdraw(amount)
 else:
     print("Transaction Did Not Go Through!")
